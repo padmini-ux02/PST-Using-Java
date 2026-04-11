b5ad1dbf07853a9145db7a6b84272543c472aaa8
Padmini <254588551+padmini-ux02@users.noreply.github.com>
2026-04-11 20:31:02 +0530

    PST

---
+++ Week 10/Task 1.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+        return nums;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        Solution obj = new Solution();
+        int[] result = obj.runningSum(nums);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+
+        sc.close();
+    }
+}
